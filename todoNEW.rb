8e737e61a084af59c34c7f56b1f4287c31df708b
Altsgamer <im4adm@gmail.com>
2017-06-06 17:58:55 +0300

    Pre-Release OOP version

--- todoNEW.rb
+++ todoNEW.rb
@@ -14,21 +14,48 @@ class TodoList
 	end
 
 	def read(n)
+		puts @todo_array[n][1]
 	end
 
-	def complete
+	def complete(n)
+		if @todo_array[n] != nil
+			if @todo_array[n][0] == 'A'
+				@todo_array[n][0] = 'D'
+				puts "Task #{n+1} is done!"
+			else
+				@todo_array[n][0] = 'A'	
+				puts "Task #{n+1} is active!"
+			end	
+		else
+			puts '! No such task!'
+		end
 		write
 	end
 
-	def delete
+	def delete(n)
+		if @todo_array[n] != nil
+			@todo_array[n] = nil
+			@todo_array = @todo_array.compact
+			puts "Task #{n+1} deleted!"
+		else
+			puts '! No such task!'
+		end		
 		write
 	end
 
-	def new
+	def newl(t)
+		@todo_array.push ['A', t]
+		puts "Active task number #{@todo_array.count} added."
 		write
 	end
 
-	def update
+	def update (n,t)
+		if @todo_array[n] != nil
+			@todo_array[n][1] = t
+			puts "Task #{n+1} updated!"
+		else
+			puts '! No such task!'
+		end
 		write
 	end
 
@@ -39,6 +66,7 @@ class TodoList
 			end
 		end
 	end
+
 end
 
 class TodoItem < Array
@@ -54,6 +82,7 @@ class CLI #CommandLineInterface
 	
 	def initialize(*arg)
 	    @arg = arg
+	    verifyARGV
 	    td = TodoList.new
 		case @arg[0]
 			when ('-h' || '--help')
@@ -61,20 +90,29 @@ class CLI #CommandLineInterface
 			when ('-l' || '--list')
 				td.list
 			when ('-r' || '--read')
-				td.read(num)
+				@num == -1 ? sysmessage('N') : td.read(@num)
 			when ('-c' || '--complete')
-				td.complete(num)
+				@num == -1 ? sysmessage('N') : td.complete(@num)
 			when ('-d' || '--delete')
-				td.delete(num)
+				@num == -1 ? sysmessage('N') : td.delete(@num)
 			when ('-n' || '--new')
-				td.newl(text)
+				@text == nil ? sysmessage('T') : td.newl(@text)
 			when ('-u' || '--update')
-				td.update(num, text)
+				if @num != -1
+					@text == nil ? sysmessage('T') : td.update(@num, @text)
+				else
+					sysmessage('N')
+				end	
 		else 
 			puts "! Unknown command!"
 		help
 		end
 	end
+
+	def verifyARGV
+		@num = @arg[1].to_i-1
+		@num != -1 ? @text = @arg[2] : @text = @arg[1]
+	end	
 
 	def help
 		puts 'Commands for todo.rb:'
@@ -82,13 +120,13 @@ class CLI #CommandLineInterface
 		puts '--read N, -r N: show the N-th record'
 		puts '--complete N, -c N: mark the N-th record as "done". If the N-th record is already "done", it should become "todo" again'
 		puts '--delete N, -d N: remove the N-th record from the list (it should disappear from the file)'
-		puts '--new some text, -n some text: creates a new record with title "some text"'
-		puts '--update N some new text, -u N some new text: updated the N-th record title with "some new text"'
+		puts '--new "some text", -n "some text": creates a new record with title "some text"'
+		puts '--update N "some new text", -u N "some new text": updated the N-th record title with "some new text"'
 		puts '--help, -h: print out this information"'
 	end
 
-	def sysmessage(text)
-		@text = text
+	def sysmessage(t)
+		@text = t
 		case @text
 			when 'S'
 				puts '! No such task!'
